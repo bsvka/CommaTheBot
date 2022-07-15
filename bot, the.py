5fa18499ae5050d04f920cec824381380a57721c
bsvka <109389712+bsvka@users.noreply.github.com>
2022-07-15 20:53:27 +0200

    V1

--- bot, the.py
+++ bot, the.py
@@ -1,59 +1,57 @@
-"""
-Example code to remove leading and trailing whitespace from Edition titles
+# example invocation
+# python bot, the.py --file ol_dump_2022-06-06.txt.gz --limit 1
 
-This script would be called from the command line like so:
-$ python making_a_bot.py --file=~/ol_dump_2020-07.txt.gz --limit=1 --dry-run=True
-
-NOTE: This script assumes the entire OpenLibrary dump is the file argument, but it is almost always faster to pass a grep-filtered dump instead.
-You can obtain dumps from https://openlibrary.org/developers/dumps
-"""
-
+from olclient.bots import AbstractBotJob
 import copy
 import gzip
 import re
 
-from olclient.bots import AbstractBotJob
-
-
-class TrimTitleJob(AbstractBotJob):
+class Job_The(AbstractBotJob):
     @staticmethod
     def needs_fixing( edition_title: str ) -> bool:
-        # [\w ]*, ?[Tt]he
-        match = re.search
+        if edition_title == None: return False
 
-        return edition_title.strip() != edition_title
+        match = re.search("^[\w ,]*, ?[Tt]he$", edition_title)
+
+        return False if(match == None) else True
+
+    @staticmethod
+    def fix_title( edition_title: str ) -> str:
+        match = re.search("^([\w ,]*), ?([Tt]he)$", edition_title)
+
+        return " ".join([match.group(2), match.group(1)])
 
     def run(self) -> None:
         self.dry_run_declaration()
 
-        comment = 'trim whitespace'
+        comment = 'foo, the -> the foo'
         with gzip.open(self.args.file, 'rb') as file:
             for row in file:
-                # extract info from the dump file and check it
                 row, json_data = self.process_row(row)
-                """ if json_data['type']['key'] != '/type/edition':
-                    continue  # this can be done faster with a grep filter, but for this example we'll do it here """
-                if not self.needs_fixing(json_data['title']):
+                if not ( json_data['type']['key'] == '/type/edition' or json_data['type']['key'] == '/type/work' ):
+                    continue
+                if not self.needs_fixing(json_data.get('title')): # .get() to avoid KeyError
                     continue
 
                 # the database may have changed since the dump was created, so call the OpenLibrary API and check again
                 olid = json_data['key'].split('/')[-1]
-                edition = self.ol.Edition.get(olid)
-                """ if edition.type['key'] != '/type/edition':
-                    continue  # skip deleted editions """
-                if not self.needs_fixing(edition.title):
+                book = self.ol.Edition.get(olid) if json_data['type']['key'] == '/type/edition' else self.ol.Work.get(olid)
+
+                if not ( book.type['key'] == '/type/edition' or book.type['key'] == '/type/work' ):
+                    continue # skip deleted books
+                if not self.needs_fixing(book.title):
                     continue
 
-                # this edition needs editing, so fix it
-                old_title = copy.deepcopy(edition.title)
-                edition.title = edition.title.strip()
-                # don't forget to log modifications!
-                self.logger.info('\t'.join([olid, old_title, edition.title]))
-                self.save(lambda: edition.save(comment=comment))
+                # this book needs editing, so fix it
+                old_title = copy.deepcopy(book.title)
+                book.title = self.fix_title(book.title)
+
+                self.logger.info(f'{olid}: "{old_title}" -> "{book.title}"')
+                self.save(lambda: book.save(comment=comment))
 
 
 if __name__ == "__main__":
-    job = TrimTitleJob()
+    job = Job_The()
 
     try:
         job.run()
